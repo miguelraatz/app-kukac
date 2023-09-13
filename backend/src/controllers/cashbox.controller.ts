fb1828a2f333784474bed9d16ed265127c081694
miguelraatz <miguelraatz_@hotmail.com>
2023-09-13 09:54:47 -0300

    feat: add controller cash box

---
+++ backend/src/controllers/cashbox.controller.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from 'express-serve-static-core';
+import CashBoxService from '../services/cashbox.service';
+class CashBoxController {
+
+  CashBoxService = new CashBoxService();
+
+  public calculateChange(req: Request, res: Response) {
+    try {
+      const { purchase, money } = req.body;
+      const result = this.CashBoxService.calculateChange(Number(purchase), Number(money));
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+export default CashBoxController;
