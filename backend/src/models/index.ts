21dc31ca4a21f0b9956f09728359ef10a8c799ba
miguelraatz <miguelraatz_@hotmail.com>
2023-09-13 09:56:55 -0300

    feat: implemented index

---
+++ backend/src/models/index.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs/promises';
+import { join } from 'path';
+
+const path = '../database/data.json';
+class Model {
+  public readJsonFile = async () => {
+    try {
+      const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+      return JSON.parse(contentFile);
+    } catch (error) {
+      return null;
+    }
+  };
+
+  public writeJsonFile = async (content) => {
+    try {
+      const completePath = join(__dirname, path);
+      await fs.writeFile(completePath, JSON.stringify(content));
+    } catch (e) {
+      console.error('Erro ao salvar o arquivo', e.message);
+      return null;
+    }
+  };
+}
+
+export default Model;
+
